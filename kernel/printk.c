1e848a81fc1cb615dfa2c53545fa7818283c53f1
Thomas Lo <thomasjinlo@gmail.com>
2026-08-26 20:59:21 -0700

    rename printf.c to printk.c

--- kernel/printk.c
+++ kernel/printk.c
@@ -1,5 +1,5 @@
 //
-// formatted console output -- printf, panic.
+// formatted console output -- printk, panic.
 //
 
 #include <stdarg.h>
@@ -16,9 +16,9 @@
 #include "proc.h"
 
 volatile int panicking = 0; // printing a panic message
-volatile int panicked = 0; // spinning forever at end of a panic
+volatile int panicked = 0;  // spinning forever at end of a panic
 
-// lock to avoid interleaving concurrent printf's.
+// lock to avoid interleaving concurrent printk's.
 static struct {
   struct spinlock lock;
 } pr;
@@ -32,7 +32,7 @@ printint(long long xx, int base, int sign)
   int i;
   unsigned long long x;
 
-  if(sign && (sign = (xx < 0)))
+  if (sign && (sign = (xx < 0)))
     x = -xx;
   else
     x = xx;
@@ -40,12 +40,12 @@ printint(long long xx, int base, int sign)
   i = 0;
   do {
     buf[i++] = digits[x % base];
-  } while((x /= base) != 0);
+  } while ((x /= base) != 0);
 
-  if(sign)
+  if (sign)
     buf[i++] = '-';
 
-  while(--i >= 0)
+  while (--i >= 0)
     consputc(buf[i]);
 }
 
@@ -61,73 +61,74 @@ printptr(uint64 x)
 
 // Print to the console.
 int
-printf(char *fmt, ...)
+printk(char *fmt, ...)
 {
   va_list ap;
   int i, cx, c0, c1, c2;
   char *s;
 
-  if(panicking == 0)
+  if (panicking == 0)
     acquire(&pr.lock);
 
   va_start(ap, fmt);
-  for(i = 0; (cx = fmt[i] & 0xff) != 0; i++){
-    if(cx != '%'){
+  for (i = 0; (cx = fmt[i] & 0xff) != 0; i++) {
+    if (cx != '%') {
       consputc(cx);
       continue;
     }
     i++;
-    c0 = fmt[i+0] & 0xff;
+    c0 = fmt[i + 0] & 0xff;
     c1 = c2 = 0;
-    if(c0) c1 = fmt[i+1] & 0xff;
-    if(c1) c2 = fmt[i+2] & 0xff;
-    if(c0 == 'd'){
+    if (c0)
+      c1 = fmt[i + 1] & 0xff;
+    if (c1)
+      c2 = fmt[i + 2] & 0xff;
+    if (c0 == 'd') {
       printint(va_arg(ap, int), 10, 1);
-    } else if(c0 == 'l' && c1 == 'd'){
+    } else if (c0 == 'l' && c1 == 'd') {
       printint(va_arg(ap, uint64), 10, 1);
       i += 1;
-    } else if(c0 == 'l' && c1 == 'l' && c2 == 'd'){
+    } else if (c0 == 'l' && c1 == 'l' && c2 == 'd') {
       printint(va_arg(ap, uint64), 10, 1);
       i += 2;
-    } else if(c0 == 'u'){
+    } else if (c0 == 'u') {
       printint(va_arg(ap, uint32), 10, 0);
-    } else if(c0 == 'l' && c1 == 'u'){
+    } else if (c0 == 'l' && c1 == 'u') {
       printint(va_arg(ap, uint64), 10, 0);
       i += 1;
-    } else if(c0 == 'l' && c1 == 'l' && c2 == 'u'){
+    } else if (c0 == 'l' && c1 == 'l' && c2 == 'u') {
       printint(va_arg(ap, uint64), 10, 0);
       i += 2;
-    } else if(c0 == 'x'){
+    } else if (c0 == 'x') {
       printint(va_arg(ap, uint32), 16, 0);
-    } else if(c0 == 'l' && c1 == 'x'){
+    } else if (c0 == 'l' && c1 == 'x') {
       printint(va_arg(ap, uint64), 16, 0);
       i += 1;
-    } else if(c0 == 'l' && c1 == 'l' && c2 == 'x'){
+    } else if (c0 == 'l' && c1 == 'l' && c2 == 'x') {
       printint(va_arg(ap, uint64), 16, 0);
       i += 2;
-    } else if(c0 == 'p'){
+    } else if (c0 == 'p') {
       printptr(va_arg(ap, uint64));
-    } else if(c0 == 'c'){
+    } else if (c0 == 'c') {
       consputc(va_arg(ap, uint));
-    } else if(c0 == 's'){
-      if((s = va_arg(ap, char*)) == 0)
+    } else if (c0 == 's') {
+      if ((s = va_arg(ap, char *)) == 0)
         s = "(null)";
-      for(; *s; s++)
+      for (; *s; s++)
         consputc(*s);
-    } else if(c0 == '%'){
+    } else if (c0 == '%') {
       consputc('%');
-    } else if(c0 == 0){
+    } else if (c0 == 0) {
       break;
     } else {
       // Print unknown % sequence to draw attention.
       consputc('%');
       consputc(c0);
     }
-
   }
   va_end(ap);
 
-  if(panicking == 0)
+  if (panicking == 0)
     release(&pr.lock);
 
   return 0;
@@ -137,15 +138,15 @@ void
 panic(char *s)
 {
   panicking = 1;
-  printf("panic: ");
-  printf("%s\n", s);
+  printk("panic: ");
+  printk("%s\n", s);
   panicked = 1; // freeze uart output from other CPUs
-  for(;;)
+  for (;;)
     ;
 }
 
 void
-printfinit(void)
+printkinit(void)
 {
   initlock(&pr.lock, "pr");
 }
